f76eb2451a66b0b7be61bfbcedb10d13c6ad4a51
fabian Becerra <william.becerra.mt@correo.usa.edu.co>
2021-12-06 22:19:05 -0500

    Reto2

---
+++ src/main/java/com/usa/ciclo4/reto2ciclo4/Reto2Ciclo4Application.java
@@ -0,0 +1,13 @@
+package com.usa.ciclo4.reto2ciclo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2Ciclo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reto2Ciclo4Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/usa/ciclo4/reto2ciclo4/model/Product.java
@@ -0,0 +1,27 @@
+package com.usa.ciclo4.reto2ciclo4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "products")
+public class Product {
+    @Id
+    private Integer id;
+    private String brand;
+    private String procesor;
+    private String os;
+    private String description;
+    private String memory;
+    private String hardDrive;
+    private boolean availability;
+    private double price;
+    private Integer quantity;
+    private String photography;
+
+}
